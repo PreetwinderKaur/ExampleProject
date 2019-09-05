189c55eb823cc6e36bb75038b453d0ea547f93b7
Preet <Preet@DESKTOP-U6ENHHG>
2019-09-05 11:19:52 -0400

    Initial commit

---
+++ src/preet.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class preet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my github example project");
+		
+		
+		System.out.println("What' your name?");
+		Scanner keyboard = new Scanner (System.in); 
+		
+		String personName = keyboard.nextLine();
+		
+		System.out.println("Hello " + personName);
+
+	}
+
+}
